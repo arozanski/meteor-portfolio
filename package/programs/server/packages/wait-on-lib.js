ffea4ff5e891052f42a51884aa1e6d43bfa4b15a
Armand Rozanski <armandrozanski@powa.com>
2014-06-19 12:40:16 +0100

    fix deployment, minor content updates

---
+++ package/programs/server/packages/wait-on-lib.js
@@ -0,0 +1,17 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+/* Package-scope variables */
+var IRLibLoader;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['wait-on-lib'] = {
+  IRLibLoader: IRLibLoader
+};
+
+})();
